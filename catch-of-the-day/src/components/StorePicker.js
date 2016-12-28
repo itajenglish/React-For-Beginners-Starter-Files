a688075fa11322724c7c916f8f2fc7fb07f7c73a
Taj English <tajenglish@gmail.com>
2016-12-28 15:14:12 -0500

    React Events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log("You changed the Url");
+    // first grab the text from the box
+    // second we're going to transiton from / to /store/:storeId
+    console.log(this.storeInput.value)
+  }
   render() {
     return(
-      <form action="" className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
       <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+      <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
       <button type="submit">Vist Store</button>
       </form>
     )
