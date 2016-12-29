0ed9fd43cf87bf07571fd5a3f6c6f95473ffb537
Taj English <tajenglish@gmail.com>
2016-12-29 01:52:17 -0500

    React State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,34 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+// import sampleFishes from '../sample-fishes'
 
 class App extends React.Component{
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    //getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish){
+    // update our state
+    const fishes = {...this.state.fishes}
+    //add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    //set state
+    this.setState({ fishes })
+  }
+
+  loadSamples() {
+    this.setState({
+      // fishes
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +37,7 @@ class App extends React.Component{
           <Header tagline="Fresh Sea Food Marget" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
