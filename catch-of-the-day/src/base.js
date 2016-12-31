b782bc23ca56d308eeddeaaafb5fabfe1b56b07c
Taj English <tajenglish@gmail.com>
2016-12-30 21:02:14 -0500

    Persisting Order State with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDMUoPUDToNsdV8jpeElhgjyPYThg0zWDA",
+    authDomain: "catch-of-the-day-taj-english.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-taj-english.firebaseio.com"
+})
+
+export default base;
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -17,7 +17,7 @@ class Order extends React.Component{
     return(
       <li key={key}>
       <span>{count}lbs {fish.name}</span>
-      <span className="price">{count * fish.price}</span>
+      <span className="price">{formatPrice(count * fish.price)}</span>
       </li>
     )
   }
